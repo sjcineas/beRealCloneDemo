2a3b74d7d025237603007a6249a8467a75e46a47
Schadrack Cineas <sjcineas@gmail.com>
2023-03-24 15:23:51 -0400

    Initial Commit

---
+++ beRealClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  beRealClone
+//
+//  Created by Schadrack Cineas on 3/24/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
